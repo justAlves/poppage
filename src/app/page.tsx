1d7e8bd5a95f75471a15b6957e7ca92bb9bca2fd
Guilherme Alves Profirio <iamalves.jpg@gmail.com>
2024-09-15 11:45:47 -0300

    feat: block first idea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,99 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-auto bg-main flex flex-col text-black">
+      <div className="xl:w-1/2 xl:h-full h-1/2 flex flex-col py-52 px-16 gap-8">
+        <h1 className="text-5xl font-black text-pink-500"><span className="text-orange-500">Pop</span>Page</h1>
+        <p className="text-2xl">Com apenas alguns cliques, você cria uma página para o seu negócio.</p>
+        <div className="flex gap-4">
+          <div className="relative inline-block p-[2px] rounded-full bg-gradient-to-br from-orange-500 to-pink-500">
+            <Button variant={"badge"}>
+              Cartão de visita digital.
+            </Button>
+          </div>
+          <div className="relative inline-block p-[2px] rounded-full bg-gradient-to-br from-orange-500 to-pink-500">
+            <Button variant={"badge"}>
+              Página de negócio.
+            </Button>
+          </div>
+          <div className="relative inline-block p-[2px] rounded-full bg-gradient-to-br from-orange-500 to-pink-500">
+            <Button variant={"badge"}>
+              Cartão de visita digital + Página de negócio.
+            </Button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="xl:w-1/2 xl:h-full h-1/2 flex flex-col justify-center gap-8 p-32">
+        <form className="bg-gray-200 rounded-xl shadow-lg border-2 flex flex-col items-center p-8 gap-4">
+          <div className="w-full flex flex-col gap-4">
+            <span className="text-lg font-bold">
+              1. Vamos começar, informe os dados do seu negócio.
+            </span>
+            <Input
+              placeholder="Digite o nome do seu negócio"
+            />
+            <Input
+              placeholder="Digite um título para a sua página"
+            />
+            <Input
+              type="file"
+              placeholder="Escolha uma imagem sua ou o logo do seu negócio"
+            />
+            <Textarea
+              placeholder="Digite uma descrição para a sua página"
+              maxLength={110}
+            />
+          </div>
+          <div className="w-full flex flex-col gap-4">
+            <span className="text-lg font-bold">
+              2. Nessa próxima etapa, informe os princípios do seu negócio. Isso ajudará a criar uma página mais personalizada (limite: 3).
+            </span>
+            <Input
+              placeholder="Digite o primeiro princípio"
+            />
+            <Input
+              placeholder="Digite o segundo princípio"
+            />
+            <Input
+              placeholder="Digite o terceiro princípio"
+            />
+          </div>
+          <div className="w-full flex flex-col gap-4">
+            <span className="text-lg font-bold">
+              3. Informe os serviços que você oferece. Assim seus clientes saberão o que esperar (limite: 3).
+            </span>
+            <Input
+              placeholder="Digite o primeiro serviço"
+            />
+            <Input
+              placeholder="Digite o segundo serviço"
+            />
+            <Input
+              placeholder="Digite o terceiro serviço"
+            />  
+          </div>
+          <div className="w-full flex flex-col gap-4">
+            <span className="text-lg font-bold">
+              Por fim, informe os meios de contato para que seus clientes possam entrar em contato com você.
+            </span>
+            <Input
+              placeholder="Digite o seu e-mail"
+            />
+            <Input
+              placeholder="Digite o seu telefone"
+            />
+            <Input
+              placeholder="Possui instagram? Informe o seu usuário"
+            />
+          </div>
+          <Button>
+            Crie por apenas R$ 50,00.
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
